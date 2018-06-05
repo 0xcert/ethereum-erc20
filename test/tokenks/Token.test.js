2c8124e3f4c6d5ace74b933374aceae08361fcfc
Luka Kacil <luka.kacil@gmail.com>
2018-06-05 14:32:29 +0200

    Replace let with constant

--- test/tokenks/Token.test.js
+++ test/tokenks/Token.test.js
@@ -3,72 +3,72 @@ const assertRevert = require('../../node_modules/@0xcert/ethereum-utils/test/hel
 
 contract('erc/Token', (accounts) => {
   let token;
-  let owner = accounts[0];
-  let recipient = accounts[1];
-  let allowedAccount = accounts[2];
-  let tokenTotalSupply = new web3.BigNumber('3e+26');
-  let tokenName = "Mock Token";
-  let tokenSymbol  = "MCK";
-  let tokenDecimals = "18";
-  let ownerSupply = new web3.BigNumber('3e+26');
+  const owner = accounts[0];
+  const recipient = accounts[1];
+  const allowedAccount = accounts[2];
+  const tokenTotalSupply = new web3.BigNumber('3e+26');
+  const tokenName = "Mock Token";
+  const tokenSymbol  = "MCK";
+  const tokenDecimals = "18";
+  const ownerSupply = new web3.BigNumber('3e+26');
 
   beforeEach(async () => {
     token = await Token.new();
   });
 
   it('has correct totalSupply after construction', async () => {
-    let actualSupply = await token.totalSupply();
+    const actualSupply = await token.totalSupply();
     assert.equal(actualSupply.toString(), tokenTotalSupply.toString());
   });
 
   it('has correct token name after construction', async () => {
-    let actualName = await token.name();
+    const actualName = await token.name();
     assert.equal(actualName, tokenName);
   });
 
   it('has correct token symbol after construction', async () => {
-    let actualSymbol = await token.symbol();
+    const actualSymbol = await token.symbol();
     assert.equal(actualSymbol, tokenSymbol);
   });
 
   it('has correct token decimals after construction', async () => {
-    let actualDecimals = await token.decimals();
+    const actualDecimals = await token.decimals();
     assert.equal(actualDecimals.toString(), tokenDecimals);
   });
 
   it('has correct owner token balance after construction', async () => {
-    let actualBalance = await token.balanceOf(owner);
+    const actualBalance = await token.balanceOf(owner);
     assert.equal(actualBalance.toString(), ownerSupply.toString());
   });
 
   it('recipient and sender have correct balances after transfer', async () => {
     await token.transfer(recipient, 100);
-    let actualSenderBalance = await token.balanceOf(owner);
-    let actualRecipientBalance = await token.balanceOf(recipient);
+    const actualSenderBalance = await token.balanceOf(owner);
+    const actualRecipientBalance = await token.balanceOf(recipient);
     assert.equal(actualSenderBalance.toString(), ownerSupply.minus(100).toString());
     assert.equal(actualRecipientBalance.toString(), '100');
   });
 
   it('emits Transfer event on transfer', async () => {
-    let { logs } = await token.transfer(recipient, 100);
-    let event = logs.find(e => e.event === 'Transfer');
+    const { logs } = await token.transfer(recipient, 100);
+    const event = logs.find(e => e.event === 'Transfer');
     assert.notEqual(event, undefined);
   });
 
   it('throws when trying to transfer more than available balance', async () => {
-    let moreThanBalance = tokenTotalSupply.plus(1);
+    const moreThanBalance = tokenTotalSupply.plus(1);
     await assertRevert(token.transfer(recipient, moreThanBalance));
   });
 
   it('returns the correct allowance amount after approval', async () => {
     await token.approve(recipient, 100);
-    let actualAllowance = await token.allowance(owner, recipient);
+    const actualAllowance = await token.allowance(owner, recipient);
     assert.equal(actualAllowance, 100);
   });
 
   it('emits Approval event after approval', async () => {
-    let { logs } = await token.approve(recipient, 100);
-    let event = logs.find(e => e.event === 'Approval');
+    const { logs } = await token.approve(recipient, 100);
+    const event = logs.find(e => e.event === 'Approval');
     assert.notEqual(event, undefined);
   });
 
@@ -81,16 +81,16 @@ contract('erc/Token', (accounts) => {
     await token.approve(recipient, 100);
     await token.approve(recipient, 0);
     await token.approve(recipient, 50);
-    let actualAllowance = await token.allowance(owner, recipient);
+    const actualAllowance = await token.allowance(owner, recipient);
     assert.equal(actualAllowance, 50);
   });
 
   it('returns correct balances after transfering from another account', async () => {
     await token.approve(allowedAccount, 100);
     await token.transferFrom(owner, recipient, 100, { from: allowedAccount });
-    let balanceOwner = await token.balanceOf(owner);
-    let balanceRecipient = await token.balanceOf(recipient);
-    let balanceAllowedAcc = await token.balanceOf(allowedAccount);
+    const balanceOwner = await token.balanceOf(owner);
+    const balanceRecipient = await token.balanceOf(recipient);
+    const balanceAllowedAcc = await token.balanceOf(allowedAccount);
     assert.equal(balanceOwner.toString(), ownerSupply.minus(100).toString());
     assert.equal(balanceAllowedAcc.toNumber(), 0);
     assert.equal(balanceRecipient.toNumber(), 100);
@@ -98,8 +98,8 @@ contract('erc/Token', (accounts) => {
 
   it('emits Transfer event on transferFrom', async () => {
     await token.approve(allowedAccount, 100);
-    let { logs } = await token.transferFrom(owner, recipient, 100, { from: allowedAccount });
-    let event = logs.find(e => e.event === 'Transfer');
+    const { logs } = await token.transferFrom(owner, recipient, 100, { from: allowedAccount });
+    const event = logs.find(e => e.event === 'Transfer');
     assert.notEqual(event, undefined);
   });
 
